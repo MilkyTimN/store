833122502fdb9cc4e458031e7a1ce5352dacc05e
zhanadiluuluabaz <antongrizman193@gmail.com>
2023-12-02 20:08:16 +0600

    11

--- src/repository/impl/ProductDetailsRepositoryImpl.java
+++ src/repository/impl/ProductDetailsRepositoryImpl.java
@@ -1,11 +1,27 @@
 package repository.impl;
 
 import model.ProductDetails;
+import repository.DbHelper;
 import repository.ProductDetailsRepository;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ProductDetailsRepositoryImpl implements ProductDetailsRepository {
+    DbHelper dbHelper = new DbHelperImpl();
     @Override
     public void save(ProductDetails productDetails) {
+        String sql = "INSERT INTO tb_productDetails (productId, brand, price, description, quantity) VALUES(?, ?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = dbHelper.getPreparedStatement(sql)) {
+            preparedStatement.setInt(1, productDetails.getProductId());
+            preparedStatement.setString(2, productDetails.getBrand());
+            preparedStatement.setDouble(3, productDetails.getPrice());
+            preparedStatement.setString(4, productDetails.getDescription());
+            preparedStatement.setInt(5, productDetails.getQuantity());
 
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+        }
     }
 }
